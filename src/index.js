3e5b7c6b0af3b9a52bf861f058e1b4b3dd8c7e9a
NoBey <786964300@qq.com>
2016-10-30 17:38:18 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { DatePicker } from 'antd';
+ReactDOM.render(<DatePicker />, document.getElementById('app'));
